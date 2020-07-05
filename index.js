dfa6117bb0cd93af923197d63c1506324ab61952
Chris hsiao <hsiao516@gmail.com>
2020-07-05 22:13:31 +0800

    [新增] 基本的註冊與登入功能

--- index.js
+++ index.js
@@ -16,39 +16,98 @@
 
     const db = firebase.firestore();
 
-    const handleUserLogin = function () {
-        const submitButton = document.getElementById('login-submit');
-        const memeberAccount = document.getElementById('memeber-account');
-        const memeberPassword = document.getElementById('memeber-password');
+    const handleUserRegister = function () {
+        const registerSubmit = document.getElementById('register-submit');
+        const registerEmail = document.getElementById('register-email');
+        const registerPassword = document.getElementById('register-password');
 
-        const userLoginObject = {
-            excute: function () {
+        const userRegisterObject = {
+            execute: function () {
                 this.handleSubmit();
             },
             handleSubmit: function () {
-                // 登入按鈕事件
-                submitButton.addEventListener('click', (e) => {
+                // 註冊按鈕事件
+                registerSubmit.addEventListener('click', (e) => {
                     e.preventDefault();
 
-                    //新增會員資料
-                    db.collection('users').add({
-                        userAccount: memeberAccount.value,
-                        userPassword: memeberPassword.value,
-                    })
-                        .then(function (docRef) {
-                            console.log('Document written with ID: ', docRef.id);
+                    firebase.auth().createUserWithEmailAndPassword(registerEmail.value, registerPassword.value)
+                        .then(() => {
+                            console.log('成功');
                         })
-                        .catch(function (error) {
-                            console.error('Error adding document: ', error);
+                        .catch((error) => {
+                            console.log(error.message);
                         });
                 });
             }
         }
+        return userRegisterObject;
+    }
 
+    const handleUserLogin = function () {
+        const loginSubmit = document.getElementById('login-submit');
+        const loginEmail = document.getElementById('login-email');
+        const loginPassword = document.getElementById('login-password');
+
+        const userLoginObject = {
+            execute: function () {
+                this.handleSubmit();
+            },
+            handleSubmit: function () {
+                // 登入按鈕事件
+                loginSubmit.addEventListener('click', (e) => {
+                    e.preventDefault();
+
+                    firebase.auth().signInWithEmailAndPassword(loginEmail.value, loginPassword.value)
+                        .then(() => {
+                            const user = firebase.auth().currentUser;
+
+                            if (user) {
+                                // User is signed in.
+                                console.log('login 成功', user);
+                            }
+                        })
+                        .catch((error) => {
+                            console.log(error.message);
+                        });
+                });
+            }
+        }
         return userLoginObject;
     }
 
-    const executeOnload = handleUserLogin();
+    const handleOnload = function (assembleObj) {
+        var userRegister = assembleObj.handleUserRegister();
+        var userLogin = assembleObj.handleUserLogin();
 
-    executeOnload.excute();
+        var apiObject = {
+            execute: function () {
+                userRegister.execute();
+                userLogin.execute();
+            }
+        }
+
+        return apiObject;
+    }
+
+    const assembleObj = {
+        handleUserRegister: handleUserRegister,
+        handleUserLogin: handleUserLogin,
+    }
+
+    const executeOnload = handleOnload(assembleObj);
+
+    executeOnload.execute();
 })()
+
+
+//新增會員資料
+// db.collection('users').add({
+//     userAccount: memeberAccount.value,
+//     userPassword: memeberPassword.value,
+// })
+//     .then(function (docRef) {
+//         console.log('Document written with ID: ', docRef.id);
+//     })
+//     .catch(function (error) {
+//         console.error('Error adding document: ', error);
+//     });
